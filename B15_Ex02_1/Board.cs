1a33b9621060f2e0eb05643b5cf83a9b7a9d60d8
yafim <ifim.vo@gmail.com>
2015-05-12 12:35:55 +0300

    setNewGame()

--- B15_Ex02_1/Board.cs
+++ B15_Ex02_1/Board.cs
@@ -11,13 +11,48 @@ namespace B15_Ex02_1
         Player playerOne, playerTwo;
         Coin[,] gameBoard;
 
+        private int size;
+        private int totalMovesLeft;
+
         // Constructor
-        public Board(int width, int height, int numberOfPlayers)
+        public Board(int i_Size, int numberOfPlayers)
         {
-            gameBoard = new Coin[width, height];
+            gameBoard = new Coin[i_Size, i_Size];
             playerOne = new Player(false, Coin.X);
             playerTwo = (numberOfPlayers == 2 ) ? new Player(false, Coin.O) : new Player(true, Coin.O);
+
+            this.size = i_Size;
+            this.totalMovesLeft = (i_Size * 2 ) - 4;
+
+            setNewGame();
             
+        }
+
+        private void setNewGame()
+        {
+            // Set all board to null.
+            for (int x = 0; x < size; x++)
+            {
+                for (int y = 0; y < size; y++)
+                {
+                    gameBoard[x, y] = Coin.Null;
+                }
+            }
+
+            // Place 4 coins in board
+            int halfBoard = size/2;
+            gameBoard[halfBoard + 1, halfBoard] = Coin.X;
+            gameBoard[halfBoard, halfBoard + 1] = Coin.X;
+
+            gameBoard[halfBoard, halfBoard] = Coin.O;
+            gameBoard[halfBoard + 1, halfBoard + 1] = Coin.O;
+
+
+        }
+
+        public string print()
+        {
+            return "";
         }
 
 
@@ -26,6 +61,8 @@ namespace B15_Ex02_1
 
     public enum Coin
     {
-        X = "X", O = "O", NULL
+        X, 
+        O, 
+        Null
     };
 }

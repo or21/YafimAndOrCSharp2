0be5e0d3c34cf960e86b3152f5a9e8f3880a8fbb
yafim <ifim.vo@gmail.com>
2015-05-12 12:08:08 +0300

    boardclass

--- B15_Ex02_1/Board.cs
+++ B15_Ex02_1/Board.cs
@@ -8,5 +8,24 @@ namespace B15_Ex02_1
 {
     class Board
     {
+        Player playerOne, playerTwo;
+        Coin[,] gameBoard;
+
+        // Constructor
+        public Board(int width, int height, int numberOfPlayers)
+        {
+            gameBoard = new Coin[width, height];
+            playerOne = new Player(false, Coin.X);
+            playerTwo = (numberOfPlayers == 2 ) ? new Player(false, Coin.O) : new Player(true, Coin.O);
+            
+        }
+
+
     }
+
+
+    public enum Coin
+    {
+        X = "X", O = "O", NULL
+    };
 }

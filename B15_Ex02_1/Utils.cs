d233e420b2954e2e0b84c32be625ee6b8c0476fd
or21 <orbrand21@gmail.com>
2015-05-13 13:30:00 +0300

    aaaa

--- B15_Ex02_1/Utils.cs
+++ B15_Ex02_1/Utils.cs
@@ -10,7 +10,7 @@ namespace B15_Ex02_1
     {
         public static void MakeMove(Coin[,] i_Board, Player i_Player, int i_NewX, int i_NewY)
         {
-            
+            //
         }
 
         public static int ParseValuesToInt(char i_InputValue)
---
+++ Test/test.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Test
+{
+    class Test
+    {
+        public static void Main()
+        {
+            char c = 'C';
+            int num = (int) c;
+            Console.WriteLine(num);
+            Console.ReadLine();
+        }
+    }
+}
